14da524e175e8902f3d8844d8c1b241ca24bec49
SValen23 <98847518+SValen23@users.noreply.github.com>
2022-09-02 15:17:05 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,124 @@
+
+/*
+// tipos de datos
+"hola meameeeee" // string
+'hola meameeeee' // string
+
+var nombrede = "valen";
+let apellidode = "simaldone";
+
+nombrede = 'Draken';
+
+const PI = 3.14;
+
+console.log(PI)
+console.log(nombrede)
+console.log(apellidode)
+
+// camelcase
+
+let nickname = 'gordon';
+
+
+console.log({"username": 'ryan',
+" Score": 70.5,
+ "Hours": 14,})
+
+1000 // number
+10.1
+-2.5
+
+// boolean
+
+true
+false
+
+// array
+
+['joe', 'ryan', 'marta']
+[1, 2, 3, 4]
+[true, false, true, true]
+*/
+
+/*
+let numerone = 100;
+let numertwo = 500;
+
+let result = numerone + numertwo;
+
+console.log(result);
+*/
+
+/*
+let passwoedDB = 'hola123'
+
+let input = 'alla23'
+
+//no funciona por que la contraseña es incorrecta
+
+let result = input == passwoedDB
+
+if (result === true) {
+    console.log('login correcto');
+}
+
+if (result == false) {
+    console.log('contraseña incorrecta');
+}
+*/
+
+/*
+let score = 10;
+
+if (score > 30){
+    console.log ('necesiras pracitcar mas')
+}
+
+else if (score > 15) {
+    console.log('estas mejorando')
+}
+
+else {
+    console.log('dedicate a otra cosa')
+}
+*/
+
+
+/*
+let typecard = 'debid card'
+
+switch(typecard){
+    case 'debid card':
+        console.log('esta es una tarjeta de debid')
+        break;
+    case 'credit card':
+        console.log('esta es una tarjeta de credit')
+        break;
+    default:
+        console.log('no card');
+}
+*/
+
+/*
+let count = 50;
+
+while(count >0){
+    console.log('hola gente');
+    count = count -1;
+}
+*/
+
+
+/*
+let names = ['ryan', 'joe', 'jorge'];
+
+for(let i = 0; i < names.length;  i++) {
+    console.log(names[i]);
+}
+*/
+
+function saludar(personName){
+    console.log('hola'+ personName)
+}
+
+saludar();
